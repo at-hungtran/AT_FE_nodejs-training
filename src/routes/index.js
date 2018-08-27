b419dc46712298a122c3b94eeebd154785ff47c7
at-hungtran <hung.tran@asiantech.vn>
2018-08-27 15:55:52 +0700

    reset master

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const photographersRoutes = require('./PhotographerRoutes');
-const pictureRoutes = require('./PictureRoutes');
-
-router.use('/photographers', photographersRoutes);
-router.use('/pictures', pictureRoutes);
-
-module.exports = router;
